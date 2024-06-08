5d32a3eb03ebd39a6cf3ef9715614c90e8f2d010
Gust4voD4nt4s <gustavo1234henrique.gd@gmail.com>
2024-06-08 19:39:16 -0300

    Inserindo conteudo dentro dos card

--- src/components/Slides/Carrousel/CarrouselSwiper.tsx
+++ src/components/Slides/Carrousel/CarrouselSwiper.tsx
@@ -6,7 +6,12 @@ import { Navigation, Pagination, Scrollbar, A11y, EffectCoverflow } from 'swiper
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-import CarrouselArrow from '@/Images/carrouselArrow.png'
+import ImageImovel from '@/Images/imovel.png'
+import Star from '@/Images/estrela_favorito.png'
+import M2 from '@/Images/m2.png'
+import Cama from '@/Images/cama.png'
+import Banheira from '@/Images/banheira.png'
+import LogoMCMV from '@/Images/logo-mcmv-2023.png'
 
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -38,17 +43,90 @@ const CarrouselSwiper = () => {
                     stretch: -33,
                     depth: 60,
                     modifier: 2,
-
+                    slideShadows: false
                 }
             }
             modules={[EffectCoverflow, Navigation]}
-            className='flex justify-center items-center relative'
+            className='
+                flex 
+                justify-center 
+                items-center 
+                relative 
+                drop-shadow-[0px_15px_7px_rgba(0,0,0,0.30)]
+            '
         >
 
             {imoveis.map((imovel) => (
                 <>
                     <SwiperSlide key={imovel} className='max-w-[250px]'>
-                        <div className='max-w-[250px] h-[413px] bg-slate-500'></div>
+                        <div
+                            className='
+                                max-w-[250px] 
+                                h-[413px] 
+                                bg-white
+                                rounded-[15px]
+                                text-start
+                            '
+                        >
+                            <Image src={ImageImovel} alt='' className='rounded-t-[15px]' />
+                            <div
+                                className='
+                                    w-full
+                                    absolute
+                                    top-2
+                                    flex
+                                    items-end
+                                    justify-between
+                                    px-2
+                                '
+                            >
+                                <div
+                                    className='
+                                        w-14 
+                                        h-4
+                                        flex
+                                        justify-center
+                                        items-center 
+                                        text-white 
+                                        font-normal 
+                                        text-[7px] 
+                                        bg-[#085942] 
+                                        rounded-3xl 
+                                        drop-shadow-[4px_4px_5px_rgba(0,0,0,0.25)]
+                                        cursor-pointer
+                                    '
+                                >
+                                    <p>NA PLANTA</p>
+                                </div>
+                                <Image
+                                    src={Star}
+                                    alt='star'
+                                    className='cursor-pointer drop-shadow-[2px_2px_5px_rgba(0,0,0,0.25)]'
+                                />
+                            </div>
+
+                            <div className='ml-2 mt-[7px]'>
+                                <h1 className='text-base'>Venda - Apartamento</h1>
+                                <p className='text-xs'>Valparaiso de goias, GO</p>
+                                <ul className='flex flex-col space-y-1 mt-5'>
+                                    <li className='flex'>
+                                        <Image src={M2} alt='Metros Quadrado' />
+                                        <p className='ml-1'>600 m2</p>
+                                    </li>
+                                    <li className='flex'>
+                                        <Image src={Cama} alt='Quarto' />
+                                        <p className='ml-1'>4 Quartos</p>
+                                    </li>
+                                    <li className='flex'>
+                                        <Image src={Banheira} alt='Banheiro' />
+                                        <p className='ml-1'>3 Banheiros</p>
+                                    </li>
+                                </ul>
+                                <Image src={LogoMCMV} alt='logo minha casa minha vida' className='absolute right-2 bottom-2 drop-shadow-[4px_4px_20px_rgba(0,0,0,0.25)]' />
+                            </div>
+
+
+                        </div>
                     </SwiperSlide>
                 </>
             ))}
