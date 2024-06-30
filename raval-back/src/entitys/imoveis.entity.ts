4d0b4104ab7424f9132118ed1c4dbe4bcb67c57b
gust4vod4nt4s <gustavo1234henrique.gd@gmail.com>
2024-06-30 03:40:21 -0300

    Created immobile entity

---
+++ raval-back/src/entitys/imoveis.entity.ts
@@ -0,0 +1,109 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+export enum RoleEnumTypeOfProperty {
+    HOME = 'casa',
+    APARTMENT = 'apartamento',
+    CONDOMINIUM = 'condominium',
+    GROUND = 'terreno'
+}
+
+export enum RoleEnumTypeOfPurchase {
+    SALE = 'venda',
+    RENT = 'aluguel',
+}
+
+@Entity('immobile')
+export class Immobile {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({
+        type: 'enum',
+        enum: RoleEnumTypeOfProperty
+    })
+    type_imomobile: RoleEnumTypeOfProperty;
+
+    @Column({
+        type: 'enum',
+        enum: RoleEnumTypeOfProperty
+    })
+    type_purchase: RoleEnumTypeOfPurchase;
+
+    @Column({
+        type: 'varchar',
+        length: 50
+    })
+    city: string;
+
+    @Column({
+        type: 'varchar',
+        length: 2
+    })
+    state: string;
+
+    @Column({
+        type: 'int'
+    })
+    value: number;
+
+    @Column({
+        type: 'varchar',
+        length: 255
+    })
+    address: string;
+
+    @Column({
+        type: 'text',
+        nullable: true
+    })
+    description?: string;
+
+    @Column({
+        type: 'int'
+    })
+    square_meters: number;
+
+    @Column({
+        type: 'int',
+    })
+    bedrooms_quantity: number;
+
+    @Column({
+        type: 'int',
+    })
+    toilet_quantity: number
+
+    @Column({
+        type: 'int',
+        nullable: true
+    })
+    garage_quantity?: number;
+
+    @Column({
+        type: 'boolean',
+        default: false,
+        nullable: true
+    })
+    recreation_area?: boolean;
+
+    @Column({
+        type: 'int',
+        nullable: true
+    })
+    pool_size?: number;
+
+    @Column({
+        type: 'boolean',
+        default: false,
+        nullable: true
+    })
+    academy?: boolean;
+
+    @Column({
+        type: 'boolean',
+        default: false,
+        nullable: true
+    })
+    plant?: boolean;
+    
+}
